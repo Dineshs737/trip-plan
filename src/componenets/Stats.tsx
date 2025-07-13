c42afd1cfa7918064d26b964feb394c464de4338
Dineshs737 <dinesh77saarck@gmail.com>
2025-07-13 19:55:59 +0530

    fix Stats.tsx

--- src/componenets/Stats.tsx
+++ src/componenets/Stats.tsx
@@ -1,7 +1,14 @@
 import type { InitialItemsType } from "../item";
 
 export default function Stats({items} : {items : InitialItemsType[]}){
-    console.log(items.length)
+    // console.log(items.length)
+    if(!items.length) return (
+        <p className="stats">
+            <em>
+                Start adding some items to your packing list
+            </em>
+        </p>
+    )
     const totalItem  : number = items.length;
     const totalPackedItems : number = items.reduce((prv:number ,item:InitialItemsType)=>{
                 return item.isPacked? prv +=1:prv
@@ -9,8 +16,10 @@ export default function Stats({items} : {items : InitialItemsType[]}){
 
     const presantage : number = totalItem > 0 ?Math.round((totalPackedItems / totalItem) * 100):0
     return(
-        <footer>
-        <em>  💼 you have {totalItem }{totalItem>1?"" :"s"} items on your list , and you already packed {totalItem} ({ presantage }%)</em>
+        <footer className="stats">
+        <em>  
+           {presantage === 100 ? "You got everything! Ready to go ✈️": `💼 you have ${totalItem }${totalItem>1?"" :"s"} items on your list , and you already packed ${totalItem} (${ presantage }%)`}
+        </em>
         </footer>
     )
 } 
