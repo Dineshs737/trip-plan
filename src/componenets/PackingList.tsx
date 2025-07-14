92f21e693d715db8fd49264c7a6039a100e8be11
Dineshs737 <dinesh77saarck@gmail.com>
2025-07-14 06:40:13 +0530

    update sort methods

--- src/componenets/PackingList.tsx
+++ src/componenets/PackingList.tsx
@@ -1,59 +1,85 @@
 import React, { useState } from "react"
 import type { InitialItemsType } from "../item"
+import initialItems from "../item"
 type DeleteItemProps = {
-    onDeleteItems : (id:number)=>void,
+    onDeleteItems: (id: number) => void,
 }
 type UpdateItemType = {
-    onUpdateItem :  (id:number)=>void,
+    onUpdateItem: (id: number) => void,
 }
-type Props = {items: InitialItemsType[] ,
+type Props = {
+    items: InitialItemsType[],
 
-}&DeleteItemProps & UpdateItemType
+} & DeleteItemProps & UpdateItemType
 
-const sortOptions : string[]  = ["sort by input order" , "sort by input order" , "sort bt packed status"]
 
-export default function PackingList({items , onDeleteItems ,onUpdateItem } : Props ){ 
-    return(
-        <div className="list">
-           <ul>
-            {
-                    items.map(({id ,description , quantity , isPacked }) => < Item onUpdateItem={onUpdateItem} onDeleteItems = {onDeleteItems} id = {id} description = {description} quantity = {quantity} isPacked = {isPacked} key={id} />)
-            }
-           </ul>
+type SortType = "input" | "description" | "packed";
 
-           {/* <SortBox  /> */}
-        </div>
-    )
-} 
+export default function PackingList({ items, onDeleteItems, onUpdateItem }: Props) {
 
-export function Item({ id, description, quantity, isPacked ,onDeleteItems  , onUpdateItem}: InitialItemsType&DeleteItemProps&UpdateItemType):React.JSX.Element{
-   
-return (
-    <li>
-        <input  type="checkbox" checked= {isPacked} onChange={():void => {
-           onUpdateItem(id)
-           
+    const [sortBy, setSortBy] = useState<SortType>("input");
+    let sortedItems : InitialItemsType[]  = []
 
-        }} />
-        <span style={isPacked ? { textDecoration : "line-through"}:{}}>
-            {quantity} {description}
-        </span>
-        <button onClick={():void=> onDeleteItems(id)}>❌</button>
-     </li>
-)
-    
-}
+    if(sortBy === "input") sortedItems = items;
+
+    if(sortBy === "description") {
+        sortedItems = items.slice().sort((a:InitialItemsType ,b:InitialItemsType)=> 
+            a.description.localeCompare(b.description))
+    }
+
+    if(sortBy === "packed"){
+        sortedItems = items.slice().sort((a:InitialItemsType ,b:InitialItemsType)=> 
+            Number(a.isPacked) - Number(b.isPacked))
+    }
 
 
 
-export function SortBox():React.JSX.Element{
     return (
-        <div>
-            <select >
-            {
-                sortOptions.map((sortMethodName:string ,index:number)=> <option key={index} value={sortMethodName} id={sortMethodName} />)
-            }
-            </select>
+        <>
+        {
+            sortedItems.length > 0 &&(
+                <div className="list">
+            <ul>
+                {
+                    sortedItems.map(({ id, description, quantity, isPacked }) => < Item onUpdateItem={onUpdateItem} onDeleteItems={onDeleteItems} id={id} description={description} quantity={quantity} isPacked={isPacked} key={id} />)
+                }
+            </ul>
+
+            <div className="actions">
+                <select
+                    value={sortBy}
+                    onChange={(event: React.ChangeEvent<HTMLSelectElement>): void =>
+                        setSortBy((currentElement: SortType) =>
+                            currentElement = event.target.value as SortType)} >
+
+                    <option value="input">Sort by input order</option>
+                    <option value="description">Sort by input description</option>
+                    <option value="packed">Sort by packed status</option>
+                </select>
+
+            </div>
+
         </div>
+            )
+        }
+        </>
     )
+}
+
+export function Item({ id, description, quantity, isPacked, onDeleteItems, onUpdateItem }: InitialItemsType & DeleteItemProps & UpdateItemType): React.JSX.Element {
+
+    return (
+        <li>
+            <input type="checkbox" checked={isPacked} onChange={(): void => {
+                onUpdateItem(id)
+
+
+            }} />
+            <span style={isPacked ? { textDecoration: "line-through" } : {}}>
+                {quantity} {description}
+            </span>
+            <button onClick={(): void => onDeleteItems(id)}>❌</button>
+        </li>
+    )
+
 }
