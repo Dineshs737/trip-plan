e26347275af7e2456eec0778fadd1394a4ab894e
Dineshs737 <dinesh77saarck@gmail.com>
2025-10-06 12:33:28 +0530

    update and fix props

--- src/layouts/App.layout.tsx
+++ src/layouts/App.layout.tsx
@@ -2,21 +2,14 @@ import Logo from "../componenets/Logo";
 import Form from "../componenets/Form";
 import PackingList from "../componenets/PackingList";
 import Stats from "../componenets/Stats";
-import useItem from "../contexts/Item.context";
 
 export function AppLayout(): React.JSX.Element {
-  const { addItem, clearList, items, updateItem, deleteItem } = useItem();
   return (
     <div className="app">
       <Logo />
-      <Form onAddItems={addItem} />
-      <PackingList
-        onClearList={clearList}
-        items={items}
-        onDeleteItems={deleteItem}
-        onUpdateItem={updateItem}
-      />
-      <Stats items={items} />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
